c6889c26c4480bcdfd828677fddf27ec639b2b61
Waury <kwy700@login3.cm.cluster>
2023-06-22 11:09:26 +0200

    Add working tests

--- test_conversion.py
+++ test_conversion.py
@@ -1,5 +1,7 @@
 from conversion import celsius_to_fahrenheit
+import pytest
 
 def test_celsius_to_fahrenheit():
     assert celsius_to_fahrenheit(20) == 68
-    assert celsius_to_fahrenheit("20") is None
+    with pytest.raises(TypeError):
+        celsius_to_fahrenheit("20")
